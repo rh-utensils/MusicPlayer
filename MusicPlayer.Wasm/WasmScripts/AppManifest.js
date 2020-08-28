51ee19f6bffced2f5bc46c867d17732dac3bcbe9
Rene Hampölz <service@hampoelz.net>
2020-08-29 00:41:24 +0200

    Simplify 'AppManifest.js'

--- MusicPlayer.Wasm/WasmScripts/AppManifest.js
+++ MusicPlayer.Wasm/WasmScripts/AppManifest.js
@@ -1,13 +1,5 @@
-if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    var UnoAppManifest = {
-        splashScreenImage: "Assets/logo.svg",
-        splashScreenColor: "#212121",
-        displayName: "RH Music"
-    };
-} else {
-    var UnoAppManifest = {
-        splashScreenImage: "Assets/logo.svg",
-        splashScreenColor: "#FFFFFF",
-        displayName: "RH Music"
-    };
-}
+var UnoAppManifest = {
+    splashScreenImage: "Assets/logo.svg",
+    splashScreenColor: (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) ? "#212121" : "#FFFFFF",
+    displayName: "RH Music"
+};
