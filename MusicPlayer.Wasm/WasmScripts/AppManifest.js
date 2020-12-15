afbf9adb9a91f367cd1de49a11088606e80e9b16
Rene Hampölz <service@hampoelz.net>
2020-12-15 18:49:20 +0100

    Cleanup Code

--- MusicPlayer.Wasm/WasmScripts/AppManifest.js
+++ MusicPlayer.Wasm/WasmScripts/AppManifest.js
@@ -1,7 +1,9 @@
 var UnoAppManifest = {
     splashScreenImage: "Assets/logo.svg",
-    splashScreenColor: (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches)
-        ? "#212121"
-        : "#FFFFFF",
-    displayName: "RH Music"
+    splashScreenColor:
+        window.matchMedia &&
+            window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "#212121"
+            : "#FFFFFF",
+    displayName: "RH Music",
 };
